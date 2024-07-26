1e01b0248feea270a9801437246db0e0b3f85457
Volodymyre <vova89.89@i.ua>
2024-07-26 18:52:11 +0300

    lessons 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
 
         var scanner = new Scanner(System.in);
 
-            System.out.println("Enter the weight of the package");
-            var weight = scanner.nextDouble();
+        System.out.println("Enter the weight of the package");
+        var weight = scanner.nextDouble();
 
-            System.out.println("Enter the price per kilogram in UAH");
-            var price = scanner.nextDouble();
+        System.out.println("Enter the price per kilogram in UAH");
+        var price = scanner.nextDouble();
 
-            var cost = weight * price;
-            System.out.println("The price of sending the parcel: " + cost + "UAH");
+        var cost = weight * price;
+        System.out.println("The price of sending the parcel: " + cost + "UAH");
 
     }
 }
