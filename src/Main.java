7a98628f2a94f7b12c7226c6a6d569941c2f44bd
Volodymyre <vova89.89@i.ua>
2024-07-26 18:47:51 +0300

    hhh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi");
+
+        var scanner = new Scanner(System.in);
+
+            System.out.println("Enter the weight of the package");
+            var weight = scanner.nextDouble();
+
+            System.out.println("Enter the price per kilogram in UAH");
+            var price = scanner.nextDouble();
+
+            var cost = weight * price;
+            System.out.println("The price of sending the parcel: " + cost + "UAH");
+
     }
 }
